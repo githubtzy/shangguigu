6c1c68f5fb3a07e62a2e2b96a8978f48a80b5c87
admin <1063221895@qq.com>
2020-07-26 16:01:29 +0800

    尚硅谷谷粒学院学习笔记，10天内学习完毕，今天第一天

---
+++ src/main/java/com/sitc/shangguigu/ShangguiguApplication.java
@@ -0,0 +1,13 @@
+package com.sitc.shangguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangguiguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangguiguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sitc/shangguigu/ShangguiguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitc.shangguigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShangguiguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
